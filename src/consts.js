91ca8d4a866b723bfe547081d357d82d3154ea66
metalwarrior665 <lukaskrivka@gmail.com>
2022-04-21 14:17:46 +0200

    hotfix place recognition selector

--- src/consts.js
+++ src/consts.js
@@ -3,7 +3,7 @@ exports.DEFAULT_TIMEOUT = 60 * 1000; // 60 sec
 exports.LISTING_PAGINATION_KEY = 'lisState';
 exports.MAX_PAGE_RETRIES = 6;
 
-exports.PLACE_TITLE_SEL = 'h1[class*="header-title-title"], h1';
+exports.PLACE_TITLE_SEL = 'h1.fontHeadlineLarge';
 exports.BACK_BUTTON_SEL = 'button[jsaction*=back], button[aria-label="Back"]';
 exports.NEXT_BUTTON_SELECTOR = '[jsaction="pane.paginationSection.nextPage"]';
 
